6a5740c0f3b2a5468b6e8a27cc84d928b98ab921
Bhanwarpreet Kaur <118596916+Bhanwar02@users.noreply.github.com>
2026-04-24 21:17:40 -0400

    Delete doclink-backend/src directory

--- doclink-backend/src/appointment/appointmentRoutes.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const { verifyToken } = require("../middleware/authMiddleware");
-const appointmentController = require("./appointmentController");
-
-router.post("/", verifyToken, appointmentController.createAppointment);
-router.get("/", verifyToken, appointmentController.getAppointments);
-router.patch("/:id/status", verifyToken, appointmentController.updateAppointmentStatus);
-
-module.exports = router;
--- doclink-backend/src/middleware/roleMiddleware.js
+++
@@ -1,7 +0,0 @@
-module.exports = function (role) {
-  return (req, res, next) => {
-    if (req.user.role !== role)
-      return res.status(403).send("Access Forbidden");
-    next();
-  };
-};
